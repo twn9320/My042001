c119c7e5209c1bec157ba1d27eb75a8e942f407b
twn9320 <twn9320@gmail.com>
2017-04-20 09:25:26 +0800

    File(Read Write)BufferReader BufferWriter

--- app/src/main/java/com/example/yvtc/my042001/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my042001/MainActivity.java
@@ -4,33 +4,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    File myFile;
+    TextView tv;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        ;
+        myFile = new File(getFilesDir()+File.separator+"mydata.txt");
     }
 
     public void clickWrite(View v){
-        File dir= getFilesDir();
-        Log.d("MFILE",dir.toString());
-        File writeFile = new File(dir.toString()+File.separator+"mydata.txt");  //File.separator為系統用的斜線 EX:data/data/file
         try {
-            FileWriter fw = new FileWriter(writeFile);
+            FileWriter fw = new FileWriter(myFile);
             BufferedWriter bw = new BufferedWriter(fw);
-            bw.write("HELLOW");
+            bw.write(editText.getText().toString());
             bw.close();
             fw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void  clickRead(View v){
+
+        try {
+            FileReader fr = new FileReader(myFile);
+            BufferedReader br = new BufferedReader(fr);
+            String str=br.readLine();
+            tv.setText(str);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
